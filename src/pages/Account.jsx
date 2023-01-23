81b87d5a4b2102aba0d5d56959eb4b73fce20f54
nithinacharya11 <nithinpacharya@gmail.com>
2023-01-23 19:43:27 +0530

    storage feature added

--- src/pages/Account.jsx
+++ src/pages/Account.jsx
@@ -1,10 +1,10 @@
 import React, { useState } from 'react';
-import {UserAuth} from '../context/AuthContext'
-import { storage } from 'firebase'
+import {UserAuth} from '../context/AuthContext';
+import {storage} from '../firebase';
+import {ref, uploadBytesResumable, getDownloadURL} from 'firebase/storage'
 
 const Account = () => {
   const { logOut, user } = UserAuth();
-  const [fileUpload, setFileUpload] = useState(null)
 
   const handleSignOut = async () => {
     try {
@@ -14,11 +14,46 @@ const Account = () => {
     }
   }
 
+  const [progress, setProgress] = useState(0);
+  const formHandler = (e) => {
+    e.preventDefault();
+    const file = e.target[0].files[0];
+    uploadFiles(file);
+  };
+
+  const uploadFiles = (file) => {
+    //
+    if (!file) return;
+    const storageRef = ref(storage, `files/${file.name}`);
+    const uploadTask = uploadBytesResumable(storageRef, file);
+
+    uploadTask.on(
+      "state_changed",
+      (snapshot) => {
+        const prog = Math.round(
+          (snapshot.bytesTransferred / snapshot.totalBytes) * 100
+        );
+        setProgress(prog);
+      },
+      (error) => console.log(error),
+      () => {
+        getDownloadURL(uploadTask.snapshot.ref).then((downloadURL) => {
+          console.log("File available at", downloadURL);
+        });
+      }
+    );
+  };
+
   return (
-    <div className='text-center mt-20 text-3xl'>
-      <p>Welcome {user?.displayName}</p>
-      <input type="file" onChange={(event) => setFileUpload(event.target.files[0])}/>
-      <button onClick={handleSignOut} className='text-lg'>Logout</button>
+    <div className='text-center mt-20'>
+      <p className='text-3xl'>Welcome {user?.displayName}</p>
+      <form onSubmit={formHandler} className='flex flex-col justify-center items-center mt-10 mb-10 gap-5'>
+        <input type="file" accept=".pdf, .docx" className="text-sm p-5 ml-10" />
+        <button type="submit" className='text-sm border rounded p-3 bg-gray-600 text-white'>Upload</button>
+      </form>
+      <hr />
+      <h2 className='mt-2'>Uploading  {progress === 100 ? 'done ✅' : `${progress}%`}</h2>
+      <button onClick={handleSignOut} className='text-md mt-20 border rounded px-3 py-1 bg-gray-900 text-white'>Logout</button>
     </div>
   )
 }
